dde18538e7e1c6cc073fb52434cf8f8a2a2aa74a
pookie <vivekkolhe694@gmail.com>
2024-03-07 17:01:41 +0530

    Revert "some stuff"
    This reverts commit 1c9e81e9db2797bf61ab8b5f940794b99b367254.

--- cmd/nyaabot/main.go
+++ cmd/nyaabot/main.go
@@ -8,7 +8,15 @@ import (
 
 	"github.com/Vivek-Kolhe/gonyaa-bot/pkg/handlers"
 	"github.com/go-telegram/bot"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load("D:\\Code\\Go Workspace\\src\\github.com\\Vivek-Kolhe\\gonyaa-bot\\.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
